8d601d2093bd91679c4f762534434408cca7e254
anhtrantdt <anhtran@tdt.asia>
2021-11-16 07:07:07 +0700

    update module class

--- addons_custom/hr_class_management/models/class_information.py
+++ addons_custom/hr_class_management/models/class_information.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import re
 
 
 class ClassInformation(models.Model):
@@ -6,7 +7,14 @@ class ClassInformation(models.Model):
     _description = "Class Information"
 
     name = fields.Char(string="Ten lop", required=True)
-    grade = fields.Char(string="Khoi", required=True)
+    grade = fields.Char(string="Khoi", compute="_compute_grade")
     mainTeacher = fields.Char(string="GVCN", required=True)
     schoolID = fields.Many2one("school.information", string="Truong", required=True)
     student_list = fields.One2many("student.information", "class_id", string="Danh sach hoc sinh")
+    # address = fields.Text(string="Dia chi", related="schoolID.address")
+
+    @api.depends("name")
+    def _compute_grade(self):
+        for record in self:
+            if record.name:
+                record.grade = re.split('[a-zA-Z]', record.name, 1)[0]
--- addons_custom/hr_student_management/models/student_information.py
+++ addons_custom/hr_student_management/models/student_information.py
@@ -9,3 +9,4 @@ class StudentInformation(models.Model):
     dateOfBirth = fields.Date(string="Ngay sinh", required=True)
     class_id = fields.Many2one("class.information", string="Lop", required=True)
     subject_list = fields.Many2many("subject.information", "relation_student_subject", "student_id", "subject_id", string="Mon hoc dang ky")
+    # mainTeacher = fields.Char(string="GVCN", related="class_id.mainTeacher")
